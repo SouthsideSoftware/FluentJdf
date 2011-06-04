fb53f906a913632ec071ebb23d2cc6a48fbb33ac
Tom Cabanski <tom@cabanski.com>
2011-06-04 10:26:47 -0500

    Use NuGet for references and don't put most libs in the lib folder as they will be brought in by the build script.  The exception is commons because it is in a private Nuget source and the command line tools cannot reference the privat source and the necessary public source at the same time, which would be required to bring down commons and its depedencies.

--- src/Jdp.Jdf/Properties/AssemblyInfo.cs
+++ src/Jdp.Jdf/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jdp.Jdf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("JDF Library")]
