b0bd17704ef21cf5064cd91baf2f0d1dc8302a6a
Tom Cabanski <tom@cabanski.com>
2011-06-21 11:27:55 -0500

    Empty interfaces to make implementation of attribute consistent and maintainable down the road.

---
+++ src/FluentJdf/LinqToJdf/IJmfCommandBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentJdf.LinqToJdf
+{
+    /// <summary>
+    /// Interface for building JMF commands.
+    /// </summary>
+    public interface IJmfCommandBuilder : IJmfNodeBuilderBase
+    {
+    }
+}
---
+++ src/FluentJdf/LinqToJdf/IJmfSubmitQueueEntryCommandBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentJdf.LinqToJdf
+{
+    /// <summary>
+    /// Interface to build submit queue entry commands.
+    /// </summary>
+    public interface IJmfSubmitQueueEntryCommandBuilder : IJmfCommandBuilder
+    {
+    }
+}
