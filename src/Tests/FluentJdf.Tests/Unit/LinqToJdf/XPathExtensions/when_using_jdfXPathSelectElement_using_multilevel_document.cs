450f247ec8c716f6136a716ef64dcda32639c786
Joe Feser <joseph.feser@gmail.com>
2011-06-25 20:21:29 -0400

    Added xpath tests for multi level documents

---
+++ src/Tests/FluentJdf.Tests/Unit/LinqToJdf/XPathExtensions/when_using_jdfXPathSelectElement_using_multilevel_document.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using FluentJdf.LinqToJdf;
+using Infrastructure.Core.Testing;
+using System.Xml.Linq;
+using l = FluentJdf.LinqToJdf;
+using x = FluentJdf.LinqToJdf.XPathExtensions;
+
+namespace FluentJdf.Tests.Unit.LinqToJdf.XPathExtensions {
+
+    [Subject(typeof(FluentJdf.LinqToJdf.XPathExtensions))]
+    public class when_using_jdfXPathSelectElement_using_multilevel_document {
+
+        static XDocument ticket;
+
+        Because of = () => ticket = l.Ticket.Load(TestDataHelper.Instance.PathToTestFile("sampleJDF.xml"));
+
+        It should_resolve_rRef_from_parent_jdf =
+            () => x.JdfXPathSelectElement(ticket, @"/JDF/JDF[@ID= 'Link0002']/ResourcePool/Component[@ID='Link0009']").ShouldNotBeNull();
+    }
+}
