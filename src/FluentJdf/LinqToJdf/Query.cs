9896588f471e8134159daa310bb9a98e3752515c
Joe Feser <joseph.feser@gmail.com>
2011-06-27 22:40:30 -0400

    Resolved Issue 7 (Query)

--- src/FluentJdf/LinqToJdf/Query.cs
+++ src/FluentJdf/LinqToJdf/Query.cs
@@ -6,7 +6,25 @@ namespace FluentJdf.LinqToJdf {
     {
 #pragma warning disable 1591
 
+        public static string Events = "Events";
+        public static string FlushQueue = "FlushQueue";
+        public static string ForceGang = "ForceGang";
+        public static string KnownControllers = "KnownControllers";
+        public static string KnownDevices = "KnownDevices";
+        public static string KnownJDFServices = "KnownJDFServices";
+        public static string KnownMessages = "KnownMessages";
+        public static string KnownSubscriptions = "KnownSubscriptions";
+        public static string NewJDF = "NewJDF";
+        public static string NodeInfo = "NodeInfo";
+        public static string Occupation = "Occupation";
+        public static string QueueEntryStatus = "QueueEntryStatus";
         public static string QueueStatus = "QueueStatus";
+        public static string RepeatMessages = "RepeatMessages";
+        public static string RequestForAuthentication = "RequestForAuthentication";
+        public static string Resource = "Resource";
+        public static string Status = "Status";
+        public static string SubmissionMethods = "SubmissionMethods";
+        public static string Track = "Track";
 
 #pragma warning restore 1591
 
