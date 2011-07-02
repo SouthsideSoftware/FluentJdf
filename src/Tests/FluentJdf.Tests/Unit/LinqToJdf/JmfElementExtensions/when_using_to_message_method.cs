6ca3af50bf8b73a9b3fa7cc2c7e2064349b92daf
Tom Cabanski <tom@cabanski.com>
2011-07-02 13:24:40 -0500

    Add test to prove that ToMessage() creates a copy of added nodes.

--- src/Tests/FluentJdf.Tests/Unit/LinqToJdf/JmfElementExtensions/when_using_to_message_method.cs
+++ src/Tests/FluentJdf.Tests/Unit/LinqToJdf/JmfElementExtensions/when_using_to_message_method.cs
@@ -12,5 +12,20 @@ namespace FluentJdf.Tests.Unit.LinqToJdf.JmfElementExtensions {
         It should_be_able_to_use_on_jmf_tree = () => new XDocument(new XElement(Element.JMF, new XElement(Element.JMF))).ToMessage().ShouldBe(typeof(FluentJdf.LinqToJdf.Message));
 
         It should_throw_argument_exception_if_root_is_not_jmf = () => Catch.Exception(() => new XDocument(new XElement("foo")).ToMessage()).ShouldBe(typeof(ArgumentException));
+
+        It should_not_have_same_xelement_instance_in_source_and_message = () => {
+            var source = new XDocument();
+            source.Add(new XElement(Element.JMF));
+            var message = source.ToMessage();
+            source.Root.ShouldNotEqual(message.Root);
+        };
+
+        It should_have_same_content_in_source_and_message = () =>
+        {
+            var source = new XDocument();
+            source.Add(new XElement(Element.JMF));
+            var message = source.ToMessage();
+            XNode.DeepEquals(source.Root, message.Root).ShouldBeTrue();
+        };
     }
 }
