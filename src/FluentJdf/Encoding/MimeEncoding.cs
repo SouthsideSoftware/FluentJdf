4f11dec411581044c078bba28d247c3a512ccd1e
Joe Feser <joseph.feser@gmail.com>
2011-07-09 19:19:29 -0400

    Mime Encoding done but need more tests. see scratchpad for more info. roundtrip works.

--- src/FluentJdf/Encoding/MimeEncoding.cs
+++ src/FluentJdf/Encoding/MimeEncoding.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Infrastructure.Core.CodeContracts;
+using System.IO;
+using Infrastructure.Core.Mime;
+using Infrastructure.Core.Helpers;
+using FluentJdf.LinqToJdf;
+using Infrastructure.Core.Logging;
 
 namespace FluentJdf.Encoding {
 
@@ -11,6 +16,11 @@ namespace FluentJdf.Encoding {
     /// </summary>
     public class MimeEncoding : IEncoding {
 
+        static ILog logger = LogManager.GetLogger(typeof(MimeEncoding));
+
+        static ITransmissionPartFactory transmissionPartFactory = new TransmissionPartFactory();
+        static FluentJdf.Encoding.EncodingFactory factory = new EncodingFactory();
+
         /// <summary>
         /// Encode a collection of parts.
         /// </summary>
@@ -18,13 +28,8 @@ namespace FluentJdf.Encoding {
         /// <returns></returns>
         public EncodingResult Encode(ITransmissionPartCollection transmissionParts) {
             ParameterCheck.ParameterRequired(transmissionParts, "transmissionParts");
-
-            //if (transmissionParts.Count() > 1) {
-            //    throw new NotSupportedException(Messages.PassThroughEncoder_Encode_CannotEncodeMoreThanOnePart);
-            //}
-            //return Encode(transmissionParts.First());
-
-            throw new NotImplementedException();
+            string contentType = null;
+            return OptimalEncode(transmissionParts, out contentType);
         }
 
         /// <summary>
@@ -34,9 +39,7 @@ namespace FluentJdf.Encoding {
         /// <returns></returns>
         public EncodingResult Encode(ITransmissionPart transmissionPart) {
             ParameterCheck.ParameterRequired(transmissionPart, "transmissionPart");
-
-            //return new EncodingResult(transmissionPart.CopyOfStream(), transmissionPart.MimeType);
-            throw new NotImplementedException();
+            return Encode(new TransmissionPartCollection() { transmissionPart });
         }
 
         /// <summary>
@@ -47,10 +50,145 @@ namespace FluentJdf.Encoding {
             ParameterCheck.ParameterRequired(stream, "stream");
             ParameterCheck.StringRequiredAndNotWhitespace(mimeType, "mimeType");
 
-            //var transmissionPartCollection = new TransmissionPartCollection();
-            //transmissionPartCollection.Add(transmissionPartFactory.CreateTransmissionPart(name, stream, mimeType, id));
-            //return transmissionPartCollection;
-            throw new NotImplementedException();
+            var transmissionPartCollection = new TransmissionPartCollection();
+
+            try {
+                int nextContentId = 1;
+
+#if MONO || !CHILKAT
+                Mime mime = new Mime(stream);
+#else
+				string mimeMessage = null;
+				Mime mime = new Mime();
+				StreamReader reader = new StreamReader(stream);
+				try
+				{
+					mimeMessage = reader.ReadToEnd();
+				} 
+				finally 
+				{
+					reader.Close();
+				}
+
+				mime.LoadMime(mimeMessage);
+#endif
+                Console.WriteLine("Parts " + mime.NumParts);
+                logger.DebugFormat("Parts {0}", mime.NumParts);
+
+                for (int partIndex = 0; partIndex < mime.NumParts; partIndex++) {
+                    Mime mimePart = mime.GetPart(partIndex);
+                    string contentId = mimePart.GetHeaderField("Content-id");
+                    if (contentId.Trim().Length == 0) {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("OAIPART_"); //TODO determine better name for mime part than OAIPART_
+                        sb.Append(DateTime.Now.Ticks.ToString());
+                        sb.Append("_");
+                        sb.Append(nextContentId.ToString());
+                        contentId = sb.ToString();
+                        nextContentId++;
+                    }
+
+                    var contentType = mimePart.ContentType;
+
+                    //content type may contain ;char-encoding.  Strip it off if found.
+                    if (contentType != null) {
+                        contentType = contentType.ToLower();
+                        string[] parts = contentType.Split(';');
+                        if (parts.Length > 1) {
+                            contentType = parts[0];
+                        }
+                    }
+
+                    //TODO We need better way to get a BodyStream from a MimePart.
+                    transmissionPartCollection.Add(transmissionPartFactory.CreateTransmissionPart(name,
+                        new MemoryStream(mimePart.GetBodyBinary()), contentType, id));
+                }
+            }
+            catch (Exception err) {
+                throw new JdfException("Error occured attempting to decode multipart\related transmission.  The error is " + err.Message, err);
+            }
+
+            if (transmissionPartCollection.Count == 0) {
+                throw new JdfException("Error occured attempting to parse through message, there are no parts to the transmission!");
+            }
+
+            return transmissionPartCollection;
+
+        }
+
+        /// <summary>
+        /// Encode a set of transmission parts into a stream suitable for 
+        /// transmission.
+        /// </summary>
+        /// <param name="parts">The parts to encode.</param>
+        /// <param name="contentType">The MIME Type of the TransmissionPartCollection contents.</param>
+        /// <returns>The stream.</returns>
+        public EncodingResult OptimalEncode(ITransmissionPartCollection parts, out string contentType) {
+            return OptimalEncode(parts, out contentType, false);
+        }
+
+        /// <summary>
+        /// Encode a set of transmission parts into a stream suitable for 
+        /// transmission with or without Binary-Encoding Attachment.
+        /// </summary>
+        /// <param name="parts">The parts to encode.</param>
+        /// <param name="contentType">The MIME Type of the TransmissionPartCollection contents.</param>
+        /// <param name="withBinaryEncodingAttachment">if true, save all Attachments with binary-encoding type. </param>
+        /// <returns>An OptimalEncodingResult with the stream and the name of the backing file store if there is one.</returns>
+        public EncodingResult OptimalEncode(ITransmissionPartCollection parts, out string contentType, bool withBinaryEncodingAttachment) {
+
+            using (Mime mime = new Mime()) {
+
+                mime.NewMultipartRelated();
+
+                foreach (var part in parts) {
+                    Mime mimePart = new Mime();
+
+                    //var encoder = factory.GetEncodingForMimeType(part.MimeType);
+                    //encoder.Encode(part);
+
+                    mimePart.SetHeaderField("Content-ID", part.Id);
+
+                    //if this is not an attachment part or the mime type is text/ something then treat as text
+                    if (part.MimeType.StartsWith("text/")) {
+                        string data = null;
+                        using (var sr = new StreamReader(part.CopyOfStream())) {
+                            data = sr.ReadToEnd();
+                        }
+                        if (part.MimeType.IndexOf("xml") > -1) {
+                            mimePart.SetBodyFromXml(data);
+                        }
+                        else {
+                            mimePart.SetBodyFromPlainText(data);
+                        }
+                    }
+                    else {
+                        //TODO optimize call to get the bytes from the stream
+                        byte[] data = null;
+                        using (var sr = new BinaryReader(part.CopyOfStream())) {
+                            data = sr.ReadBytes((int)part.CopyOfStream().Length);
+                        }
+                        mimePart.SetBodyFromBinary(data);
+
+                        if (withBinaryEncodingAttachment) {
+                            mimePart.EncodingType = Mime.MimeEncoding.Binary;
+                        }
+                        else {
+                            mimePart.EncodingType = Mime.MimeEncoding.Base64;
+                        }
+                    }
+
+                    mimePart.ContentType = part.MimeType;
+                    mime.AppendPart(mimePart);
+                }
+
+                contentType = mime.ContentType;
+
+                mime.CreateDefaultType();
+
+                //TODO see if there is a more optimal way to get the stream from the mime.
+                return new EncodingResult(mime.GetMimeStream(), contentType);
+            }
         }
     }
 }
---
+++ src/Tests/FluentJdf.Tests/Unit/Encoding/MimeEncoding/when_decoding_mimeMulti_part.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using FluentJdf.Configuration;
+using FluentJdf.Encoding;
+using Infrastructure.Core.Helpers;
+using Infrastructure.Core.Testing;
+using Machine.Specifications;
+
+namespace FluentJdf.Tests.Unit.Encoding.MimeEncoding {
+
+    [Subject(typeof(FluentJdf.Encoding.MimeEncoding))]
+    public class when_decoding_mimeMulti_part {
+
+        static ITransmissionPartCollection transmissionPartCollection;
+        static long originalStreamLength;
+        static Stream stream;
+
+        Establish context = () => {
+            FluentJdf.Configuration.FluentJdfLibrary.Settings.ResetToDefaults();
+            stream = TestDataHelper.Instance.GetTestStream("mimeMultipart.txt");
+            originalStreamLength = stream.Length;
+        };
+
+        Because of = () => transmissionPartCollection =
+                           new FluentJdf.Encoding.MimeEncoding().Decode("test", stream,
+                                                                               MimeTypeHelper.MimeMultipartMimeType);
+
+        It should_contain_three_parts = () => transmissionPartCollection.Count.ShouldEqual(3);
+
+        It should_have_a_first_part_of_jmf = () => transmissionPartCollection.First().MimeType.ShouldEqual("application/vnd.cip4-jmf+xml");
+
+        //Not sure how to get to the message or ticket that I see in Dump()
+        //It should_have_a_first_part_of_jmf_that_is_valid_message = () => transmissionPartCollection.First().;
+
+        It should_have_a_second_part_of_jdf = () => transmissionPartCollection.Skip(1).First().MimeType.ShouldEqual("application/vnd.cip4-jdf+xml");
+
+        //It should_have_a_second_part_of_jdf_that_is_valid_ticket = () => transmissionPartCollection;
+
+        It should_have_a_third_part_of_jpg = () => transmissionPartCollection.Skip(2).First().MimeType.ShouldEqual("image/jpg");
+
+        It should_have_a_jpg_length_of_17913 = () => transmissionPartCollection.Skip(2).First().CopyOfStream().Length.ShouldEqual(17913);
+
+    }
+}
