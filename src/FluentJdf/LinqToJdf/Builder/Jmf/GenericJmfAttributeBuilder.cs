107d2934a37d77a3ab16bc9c7bf55bdda992a6e5
Joe Feser <joseph.feser@gmail.com>
2011-07-09 10:30:19 -0400

    More cleanup

--- src/FluentJdf/LinqToJdf/Builder/Jmf/GenericJmfAttributeBuilder.cs
+++ src/FluentJdf/LinqToJdf/Builder/Jmf/GenericJmfAttributeBuilder.cs
@@ -5,18 +5,17 @@ using System.Text;
 using System.Xml.Linq;
 using Infrastructure.Core.CodeContracts;
 
-namespace FluentJdf.LinqToJdf.Builder.Jmf
-{
+namespace FluentJdf.LinqToJdf.Builder.Jmf {
     /// <summary>
     /// Builder for setting attributes on generic JMF element.
     /// </summary>
-    public class GenericJmfAttributeBuilder : JmfAttributeBuilderBase
-    {
+    public class GenericJmfAttributeBuilder : JmfAttributeBuilderBase {
         /// <summary>
         /// Constructor.
         /// </summary>
         /// <param name="jmfBuilderBase"></param>
-        internal GenericJmfAttributeBuilder(JmfBuilderBase jmfBuilderBase) : base(jmfBuilderBase) {}
+        internal GenericJmfAttributeBuilder(JmfBuilderBase jmfBuilderBase) : base(jmfBuilderBase) {
+        }
 
         /// <summary>
         /// Sets an attribute value.
