bec206291cc4a8ffd6270b2b426826c30973ea40
Joe Feser <joseph.feser@gmail.com>
2011-07-09 15:08:20 -0400

    Encoding file cleanup before adding mime support.

--- src/FluentJdf/Encoding/IEncoding.cs
+++ src/FluentJdf/Encoding/IEncoding.cs
@@ -4,24 +4,26 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace FluentJdf.Encoding
-{
+namespace FluentJdf.Encoding {
     /// <summary>
     /// Encodes and decodes JDF transmissions.
     /// </summary>
     public interface IEncoding {
+        
         /// <summary>
         /// Encode a collection of transmission parts to a stream.
         /// </summary>
         /// <param name="transmissionParts"></param>
         /// <returns></returns>
         EncodingResult Encode(ITransmissionPartCollection transmissionParts);
+        
         /// <summary>
         /// Encode a single transmission part to a stream.
         /// </summary>
         /// <param name="transmissionPart"></param>
         /// <returns></returns>
         EncodingResult Encode(ITransmissionPart transmissionPart);
+        
         /// <summary>
         /// Decode a stream with a mime type to a collection
         /// of transmission parts.
