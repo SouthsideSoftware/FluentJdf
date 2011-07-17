aef63bc01f9ff093b2d0ff8fb066b1a8e9bc3bfd
Joe Feser <joseph.feser@gmail.com>
2011-07-17 02:23:30 -0400

    Fix compile issue. Added Mangler

---
+++ src/FluentJdf/Transmission/FileSpecUrlMangler.cs
@@ -0,0 +1,69 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace FluentJdf.Transmission {
+//    /// <summary>
+//    /// Provides methods for converting old URLs in file specs to new URLs based on some rule.
+//    /// </summary>
+//    public class FileSpecUrlMangler {
+
+//        /// <summary>
+//        /// Given a HybridDictionary of mappings between old and new URLs, converts the URL
+//        /// in every FileSpec in the JdfTree to the appropriate new URL.
+//        /// </summary>
+//        /// <param name="jdfTree">The JdfTree to process</param>
+//        /// <param name="urlMapping">A HybridDictionary keyed by existing URLs mapping to new System.Uri</param>
+//        /// <param name="mustMapCIDs">If <i>true</i> throws an exception if an existing URL starts with CID and
+//        /// no mapping has been provided for that CID.</param>
+//        /// <exception cref="NoUrlMappingException">Thrown if mustMapCIDs is true and no mapping was supplied for a CID-based URL.</exception>
+//        public static void MapFileSpecUrls(JdfTree jdfTree, IDictionary<string,string> urlMapping, bool mustMapCIDs) {
+//            ElementList fileSpecs = jdfTree.Root.GetElementsByName("FileSpec");
+
+//            foreach (Element fileSpec in fileSpecs) {
+//                //HACK: CIP4 messed up the case of CID so I'm ToLower()ing everything.
+//                if (fileSpec.Attributes.IsAttributeNonNull("URL")) {
+//                    string oldUrl = fileSpec.Attributes["URL"].ToString().ToLower();
+//                    if (urlMapping.Contains(oldUrl)) {
+//                        jdp.jdf.Attribute urlAttribute = fileSpec.Attributes["URL"];
+//                        urlAttribute.SetValue(((Uri)urlMapping[oldUrl]).AbsoluteUri);
+//                    }
+//                    else {
+//                        if (oldUrl.ToLower().StartsWith("cid:") && mustMapCIDs) {
+//                            OAIException.Throw(new NoUrlMappingException("mustMapCIDs is true and no mapping supplied for URL " + oldUrl));
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+//        /// <summary>
+//        /// Given a HybridDictionary of mappings between old and new URLs, converts the URL
+//        /// in every Preview in the JdfTree to the appropriate new URL.
+//        /// </summary>
+//        /// <param name="jdfTree">The JdfTree to process</param>
+//        /// <param name="urlMapping">A HybridDictionary keyed by existing URLs mapping to new System.Uri</param>
+//        /// <param name="mustMapCIDs">If <i>true</i> throws an exception if an existing URL starts with CID and
+//        /// no mapping has been provided for that CID.</param>
+//        /// <exception cref="NoUrlMappingException">Thrown if mustMapCIDs is true and no mapping was supplied for a CID-based URL.</exception>
+//        public static void MapPreviewUrls(JdfTree jdfTree, IDictionary<string, string> urlMapping, bool mustMapCIDs) {
+//            ElementList previews = jdfTree.Root.GetElementsByName("Preview");
+
+//            foreach (Element preview in previews) {
+//                if (preview.Attributes.IsAttributeNonNull("URL")) {
+//                    string oldUrl = preview.Attributes["URL"].ToString().ToLower();
+//                    if (urlMapping.Contains(oldUrl)) {
+//                        jdp.jdf.Attribute urlAttribute = preview.Attributes["URL"];
+//                        urlAttribute.SetValue(((Uri)urlMapping[oldUrl]).AbsoluteUri);
+//                    }
+//                    else {
+//                        if (oldUrl.ToLower().StartsWith("cid:") && mustMapCIDs) {
+//                            OAIException.Throw(new NoUrlMappingException("mustMapCIDs is true and no mapping supplied for URL " + oldUrl));
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ src/Infrastructure/Infrastructure.Core/ReadOnlyDictionary.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Core {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class ReadOnlyDictionary<TKey, TValue> : IDictionary<TKey, TValue> {
+        private IDictionary<TKey, TValue> _dictionary;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ReadOnlyDictionary() {
+            _dictionary = new Dictionary<TKey, TValue>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dictionary"></param>
+        public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary) {
+            _dictionary = dictionary;
+        }
+
+        #region IDictionary<TKey,TValue> Members
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(TKey key, TValue value) {
+            throw new NotSupportedException("This dictionary is read-only");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool ContainsKey(TKey key) {
+            return _dictionary.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICollection<TKey> Keys {
+            get {
+                return _dictionary.Keys;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Remove(TKey key) {
+            throw new NotSupportedException("This dictionary is read-only");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetValue(TKey key, out TValue value) {
+            return _dictionary.TryGetValue(key, out value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICollection<TValue> Values {
+            get {
+                return _dictionary.Values;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TValue this[TKey key] {
+            get {
+                return _dictionary[key];
+            }
+            set {
+                throw new NotSupportedException("This dictionary is read-only");
+            }
+        }
+
+        #endregion
+
+        #region ICollection<KeyValuePair<TKey,TValue>> Members
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(KeyValuePair<TKey, TValue> item) {
+            throw new NotSupportedException("This dictionary is read-only");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Clear() {
+            throw new NotSupportedException("This dictionary is read-only");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(KeyValuePair<TKey, TValue> item) {
+            return _dictionary.Contains(item);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
+            _dictionary.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Count {
+            get {
+                return _dictionary.Count;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsReadOnly {
+            get {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Remove(KeyValuePair<TKey, TValue> item) {
+            throw new NotSupportedException("This dictionary is read-only");
+        }
+
+        #endregion
+
+        #region IEnumerable<KeyValuePair<TKey,TValue>> Members
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+            return _dictionary.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            return (_dictionary as System.Collections.IEnumerable).GetEnumerator();
+        }
+
+        #endregion
+    }
+}
