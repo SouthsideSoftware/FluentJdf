1e7a4ee789e513367553f57e6e6b56695ad4aff2
Joe Feser <joseph.feser@gmail.com>
2011-08-01 10:00:19 -0400

    Missed a few ignored tests. Found because of Team City. My bad.

--- src/Tests/FluentJdf.Tests/Unit/Transmission/FileTransmissionConfig/when_replacing_variables_in_windows_style_paths.cs
+++ src/Tests/FluentJdf.Tests/Unit/Transmission/FileTransmissionConfig/when_replacing_variables_in_windows_style_paths.cs
@@ -17,17 +17,17 @@ namespace FluentJdf.Tests.Unit.Transmission.FileTransmissionConfig {
         It should_properly_replace_var_with_slash_at_end_of_path =
             () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\fi\${var}", "var", @"\replacement\").ShouldEqual(@"file://c:\foo\fi\replacement\");
 
-        [Ignore("Right now we don't handle embedded forward and back slashed in the replacment variable")] It should_properly_replace_var_with_slash_at_start_of_path =
-            () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"${var}\foo\fi\", "var", @"\replacement\").ShouldEqual(@"\replacement\foo\fi\");
+        It should_properly_replace_var_with_slash_at_start_of_path =
+           () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"${var}\foo\fi\", "var", @"\replacement\").ShouldEqual(@"\replacement\foo\fi\");
 
-        [Ignore("Right now we don't handle embedded forward and back slashed in the replacment variable")] It should_properly_replace_var_with_slash_before_ending_slash =
-            () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\fi\${var}\", "var", @"\replacement\").ShouldEqual(@"file://c:\foo\fi\replacement\");
+        It should_properly_replace_var_with_slash_before_ending_slash =
+           () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\fi\${var}\", "var", @"\replacement\").ShouldEqual(@"file://c:\foo\fi\replacement\");
 
-        [Ignore("Right now we don't handle embedded forward and back slashed in the replacment variable")] It should_properly_replace_var_with_slash_in_middle_of_path =
-            () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\${var}\fi\", "var", @"\replacement\").ShouldEqual(@"file://c:\foo\replacement\fi\");
+        It should_properly_replace_var_with_slash_in_middle_of_path =
+           () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\${var}\fi\", "var", @"\replacement\").ShouldEqual(@"file://c:\foo\replacement\fi\");
 
-        [Ignore("Right now we don't handle embedded forward and back slashed in the replacment variable")] It should_properly_replace_var_with_slash_when_appears_twice_in_path =
-            () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\${var}\fi\${var}", "var", @"\replacement\").ShouldEqual(@"file://c:\foo\replacement\fi\replacement\");
+        It should_properly_replace_var_with_slash_when_appears_twice_in_path =
+           () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\${var}\fi\${var}", "var", @"\replacement\").ShouldEqual(@"file://c:\foo\replacement\fi\replacement\");
 
         It should_properly_replace_when_appears_twice_in_path =
             () => FluentJdf.Transmission.FileTransmissionConfig.ReplaceVar(@"file://c:\foo\${var}\fi\${var}", "var", "replacement").ShouldEqual(@"file://c:\foo\replacement\fi\replacement");
